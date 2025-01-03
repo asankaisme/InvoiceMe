ef37b847d8f9a3e5353440a2297405a157c93662
Asanka CRK <asankacrk@cbsl.lk>
2025-01-03 13:57:16 +0530

    Sample component created

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,5 @@
-<div>
-    {{-- The whole world belongs to you. --}}
+<div style="text-align: center">
+    <button class="button btn-lg btn-primary" wire:click="increment">+</button>
+    <button class="button btn-lg btn-warning" wire:click="decrement">-</button>
+    <h1>{{ $count }}</h1>
 </div>
